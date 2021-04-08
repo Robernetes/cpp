54080fe7289b0c5d11e732325085ad289611f16b
raagus <raaguilarm@gmail.com>
2021-04-08 03:21:47 -0500

    Matriz transpuesta

---
+++ matrizTranspuesta.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main(){
+    int a[3][3], b[3][3], n = 3;
+
+    for (int i = 0; i < n ; i++) {
+        for(int j = 0; j < n; j++){
+            cout << "Ingresa los valores de la matriz [" << i+1 << "][" << j+1 << "]: ";
+            cin >> a[i][j];
+        }
+    }
+   
+
+    for (int i = 0; i < n ; i++) {
+        for(int j = 0; j < n; j++){
+            b[i][j] = a[j][i];
+        }
+    }
+    
+    cout << endl;
+
+    //Imprime la matriz final a
+    for (int i = 0; i < n ; i++) {
+        for(int j = 0; j < n; j++){
+            cout << a[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    cout << "**********************************************************" << endl;
+
+    //Imprime la matriz final transpuesta b
+    for (int i = 0; i < n ; i++) {
+        for(int j = 0; j < n; j++){
+            cout << b[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+
+
+    system("pause");
+
+    return 0;
+}
